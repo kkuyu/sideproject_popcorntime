043b558148d5f87248d328641045477f3765f7cb
kkuyu <devils67@naver.com>
2019-04-02 13:37:01 +0900

    import header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Header.css";
 
 export default () => (
 	<>
-		<header>
+		<header className="nav">
 			<ul>
 				<li><a href="/">Movies</a></li>
 				<li><a href="/tv">TV</a></li>
