a0de2cf213935c187fde7148d6e96223fe758b2b
kkuyu <devils67@naver.com>
2019-04-04 11:37:26 +0900

    result fix

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -5,7 +5,7 @@ import { movieApi, tvApi } from "api";
 export default class extends React.Component{
 	constructor(props){
 		super(props);
-		const { location: { pathname } } = this.props;
+		const { location: { pathname } } = props;
 		this.state = {
 			result: null,
 			error: null,
@@ -19,7 +19,7 @@ export default class extends React.Component{
 			match: { params: { id } },
 			history: { push }
 		} = this.props;
-		const { isMovie } = this.setState;
+		const { isMovie } = this.state;
 		const parsedId = parseInt(id);
 		if(isNaN(parsedId)){
 			return push("/");
@@ -27,16 +27,14 @@ export default class extends React.Component{
 		let result = null;
 		try{
 			if (isMovie) {
-				const request = await movieApi.movieDetail(parsedId);
-				result = request.data;
+				({ data: result } = await movieApi.movieDetail(parsedId));
 			} else {
-				const request = await tvApi.showDetail(parsedId);
-				result = request.data;
+				({ data: result } = await tvApi.showDetail(parsedId));
 			}
 		}catch(error){
 			this.setState({ error: "Can't find anything." });
 		}finally{
-			this.setState({ loading: false });
+			this.setState({ loading: false, result });
 		}
 	}
 
