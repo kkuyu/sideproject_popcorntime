d4770cd14854a7b40f37c5f26a10924838cd5a44
kkuyu <devils67@naver.com>
2019-04-04 17:01:49 +0900

    movie section add

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,8 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Sections";
 
-const HomePresenter = ({ nowPlaying, upcomfing, popular, loading, error }) => null;
+const Container = styled.div`
+	padding: 0 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => (
+	loading ? null : (
+		<Container>
+			{ nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing"> { nowPlaying.map(movie => movie.title) } </Section> }
+			{ upcoming && upcoming.length > 0 && <Section title="Upcomfing Movies"> { upcoming.map(movie => movie.title) } </Section> }
+			{ popular && popular.length > 0 && <Section title="Popular Movies"> { popular.map(movie => movie.title) } </Section> }
+		</Container>
+	)
+);
 
 HomePresenter.propTypes = {
 	nowPlaying: PropTypes.array,
