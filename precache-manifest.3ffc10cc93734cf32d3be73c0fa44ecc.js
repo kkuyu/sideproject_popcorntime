3d8deb966c1c077b0fcc731d305be27866b82ee2
kkuyu <devils67@naver.com>
2019-04-06 19:50:13 +0900

    Updates

---
+++ precache-manifest.3ffc10cc93734cf32d3be73c0fa44ecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/sideproject_popcorntime/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "f92c60e9d40c9f6fd8bf",
+    "url": "/sideproject_popcorntime/static/js/runtime~main.68321149.js"
+  },
+  {
+    "revision": "5fad82f64068ee72d2c3",
+    "url": "/sideproject_popcorntime/static/js/main.2ac4612e.chunk.js"
+  },
+  {
+    "revision": "d2aeae0fe0db1ae96b7b",
+    "url": "/sideproject_popcorntime/static/js/2.97ddc3e4.chunk.js"
+  },
+  {
+    "revision": "7df45dd2fb3ae2487170ade4f55be545",
+    "url": "/sideproject_popcorntime/index.html"
+  }
+];
